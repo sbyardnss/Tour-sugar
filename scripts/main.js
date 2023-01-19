5e2c76ab996d7bc5608d95dd319388449a32be0a
Mary Williams <sbyardnss@gmail.com>
2023-01-19 15:35:50 -0600

    initial setup

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+//html layout
+
+const mainBody = document.querySelector("#container");
+
+const siteHTML = `
+<h1>Tour some sugar on me</h1>
+<article class="">
+    <section>
+        <h2>Bookings</h2>
+    </section>
+</article>
+<article>
+    <section>
+        <h2>Venues</h2>
+    </section>
+    <section>
+        <h2>Bands</h2>
+    </section>
+</article>
+`
+
+mainBody.innerHTML = siteHTML
